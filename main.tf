36c4177e5c72aa1f0c733db72dd3c73c3debeadd
Bhawesh Kumar <Bhawesh6.Kumar@ril.com>
2026-06-26 18:20:23 +0530

    Last mile4

--- main.tf
+++ main.tf
@@ -2,14 +2,16 @@ resource "azurerm_resource_group" "milkha" {
   name     = "milkha002"
   location = "central india"
 }
-resource "azurerm_storege_account" "riha"{
-name ="hana"
-resouce_group_name = azurerm_resource_group.milkha.name
-location = azurerm_resource_group.milkha.location
-account_tier = "Standard"
-account_replication_type = "LRS"
+resource "azurerm_storage_account" "riha" {
+  name                     = "riha123storage11"
+  resource_group_name      = azurerm_resource_group.milkha.name
+  location                 = azurerm_resource_group.milkha.location
+  account_tier            = "Standard"
+  account_replication_type = "LRS"
 }
-resource "azurerm_storage_contaner" "selena"{
-name = "gomez"
-storage_account_name = azurerm_storege_account.riha.name
+
+resource "azurerm_storage_container" "selena" {
+  name                  = "selenagomez"
+  storage_account_name  = azurerm_storage_account.riha.name
+  container_access_type = "private"
 }
