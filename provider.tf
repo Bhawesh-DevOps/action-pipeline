3d582ec03c58c5d2719e2ea2158239a44ab7d11c
Bhawesh Kumar <Bhawesh6.Kumar@ril.com>
2026-06-26 17:56:40 +0530

    Last mile

--- provider.tf
+++ provider.tf
@@ -1,19 +1,19 @@
 terraform {
-required_providers {
+  required_providers {
     azurerm = {
-     source = "hashicorp/azurerm"
-     version = "4.78.0"
+      source  = "hashicorp/azurerm"
+      version = "4.78.0"
+    }
   }
- }
-backend "azurerm" {
-resource_group_name = "numero"
-storage_account_name = "hulku"
-storage_container_name = "selenagomez"
-key = "tfstatefile"
-}
+  backend "azurerm" {
+    resource_group_name  = "numero"
+    storage_account_name = "hulku"
+    container_name       = "selenagomez"
+    key                  = "tfstatefile"
+  }
 
 }
-provider azurerm {
-   features{}
-   subscription_id = "2b0a7e07-53fd-47f9-85cf-6fbc0364e35f"
+provider "azurerm" {
+  features {}
+  subscription_id = "2b0a7e07-53fd-47f9-85cf-6fbc0364e35f"
 }
