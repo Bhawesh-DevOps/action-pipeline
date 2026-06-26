2fd1956efd6cadbe8c3bf0cae89a8a1751751e44
Bhawesh Kumar <Bhawesh6.Kumar@ril.com>
2026-06-26 17:44:08 +0530

    addnewpipeline

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "milkha" {
+    name = "milkha001"
+    location = "central india"
+}
---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+required_providers {
+    azurerm = {
+     source = "4.78.0"
+     version = "hashicorp/azurerm"
+  }
+ }
+backend "azurerm" {
+resource_group_name = "numero"
+storage_account_name = "hulku"
+storage_container_name = "selenagomez"
+key = "tfstatefile"
+}
+
+}
+provider azurerm {
+   features{}
+   subscription_id = "2b0a7e07-53fd-47f9-85cf-6fbc0364e35f"
+}
