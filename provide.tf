e0d9748c420e2293c42a7efd66836cc041218150
Bhawesh Kumar <Bhawesh6.Kumar@ril.com>
2026-06-26 16:24:49 +0530

    add

---
+++ provide.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.77"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "messi0077"
+    storage_account_name = "messistg1"
+    container_name       = "container007"
+    key                  = "tfstatefile"
+  }
+}
+
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = "2b0a7e07-53fd-47f9-85cf-6fbc0364e35f"
+}
